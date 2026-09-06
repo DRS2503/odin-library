0b544b0464ce5c8f94a5de4f3f82b264885765e3
DerekSchramm <drschramm33@gmail.com>
2026-09-06 12:32:04 -0500

    Re-created addBookToTable to makeTable
    The old function build the table by just adding on to the HTML.
    This caused problems when removing elements because the myLibrary
    array and html table were acting independently. This change makes
    the table rebuild based on myLibrary.

--- scripts.js
+++ scripts.js
@@ -11,36 +11,53 @@ function book(title, author, year){
 function addBookToLibrary(title, author, year){
     const book1 = new book(title, author, year);
     myLibrary.push(book1);
-    addBookToTable(myLibrary[myLibrary.length - 1]);
+    makeTable();
 }
 
-function addBookToTable(book){
-    console.log(book);
+function makeTable(){
+    let numIndex = 0;
     const tableBody = document.querySelector('tbody');
-    const tableRow = document.createElement('tr');  
-    tableBody.append(tableRow);
-    const colOneData = document.createElement('td');
-    const buttonRead = document.createElement('button');
-    buttonRead.textContent = 'Read';
-    buttonRead.className = 'read-button';
-    const buttonRemove = document.createElement('button');
-    buttonRemove.textContent = 'Remove';
-    buttonRemove.className = 'remove-button';
-    tableRow.append(colOneData);
-    colOneData.append(buttonRead, buttonRemove);
-    
-    const value = Object.values(book);
-    for(let i = 0; i < 4; i++){
-        console.log(value[i]);
-        const tabledata1 = document.createElement('td');
-        tabledata1.textContent = value[i];
-        tableRow.append(tabledata1);
+    tableBody.innerHTML = "";
+
+    for(let j = 0; j < myLibrary.length; j++){
+        const tableRow = document.createElement('tr');  
+        tableBody.append(tableRow);
+
+        const colOneData = document.createElement('td');
+
+        const buttonRead = document.createElement('button');
+        buttonRead.textContent = 'Read';
+        buttonRead.className = 'read-button';
+        buttonRead.addEventListener('click', handleButtonClickRead);
+
+        const buttonRemove = document.createElement('button');
+        buttonRemove.textContent = 'Remove';
+        buttonRemove.className = 'remove-button';
+        buttonRemove.id = numIndex;
+        numIndex += 1; 
+        buttonRemove.addEventListener('click', handleButtonClickRemove);
+
+        tableRow.append(colOneData);
+        colOneData.append(buttonRead, buttonRemove);
+
+        const value = Object.values(myLibrary[j]);
+        for(let i = 0; i < 4; i++){
+            const tabledata1 = document.createElement('td');
+            tabledata1.textContent = value[i];
+            tableRow.append(tabledata1);
+        }
     }
 }
 
+function handleButtonClickRead(event){
+    event.currentTarget.style.backgroundColor = 'green';
+}
 
-
-
+function handleButtonClickRemove(event){
+    const item = event.currentTarget.id;
+    myLibrary.splice(item, 1);
+    makeTable();
+}
 
 
 addBookToLibrary("Harry Potter and the Half-Blood Prince", "J.K. Rowling", 2005);
