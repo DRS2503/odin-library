b0c5e93129a0bfbd2505cb553647ca8cc9f444fe
DRS2503 <drschramm33@gmail.com>
2026-09-18 11:57:38 -0500

    Remade entire scripts.js using classes instead of closures

--- scripts.js
+++ scripts.js
@@ -1,91 +1,82 @@
 const myLibrary = [];
-let isClicked = false;
 
-function book(title, author, year){
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.year = year;
-
+class Book{
+    constructor(title, author, year, read){
+        this.Id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.read = read;
+    }
 }
 
-function addBookToLibrary(title, author, year){
-    const book1 = new book(title, author, year);
-    myLibrary.push(book1);
-    makeTable();
-}
+//New Book Button
+const dialog = document.querySelector('dialog');
+const newBookBtn = document.getElementById('new-book');
+newBookBtn.addEventListener('click', () => {
+    dialog.showModal();
+})
 
-function makeTable(){
-    let numIndex = 0;
+
+//Submit Button
+const submitBtn = document.getElementById('submit-btn');
+const form = document.querySelector('form');
+submitBtn.addEventListener('click', () => {
+    const formValues = new FormData(form);
+    if(formValues.get('title') == '' || formValues.get('author') == '' || formValues.get('year') == '' || formValues.get('read') == ''){
+        return
+    }
+    const book = new Book(formValues.get('title'), formValues.get('author'), formValues.get('year'), formValues.get('read'));
+    myLibrary.push(book);
+    form.reset();
+    render();
+})
+
+
+//Close Button
+const closeBtn = document.getElementById('close-button');
+closeBtn.addEventListener('click', () =>{
+    dialog.close();
+})
+
+
+function render(){
     const tableBody = document.querySelector('tbody');
-    tableBody.innerHTML = "";
+    tableBody.textContent = ""
+    const librarySize = myLibrary.length;
 
-    for(let j = 0; j < myLibrary.length; j++){
-        const tableRow = document.createElement('tr');  
+    //Make each row
+    for(let row = 0; row < librarySize; row++){
+        const tableRow = document.createElement('tr');
         tableBody.append(tableRow);
 
-        const colOneData = document.createElement('td');
+        //Remove Button
+        const tableData = document.createElement('td');
+        const removeBtn = document.createElement('button');
+        removeBtn.id = row;
+        removeBtn.textContent = "Remove";
+        removeBtn.addEventListener('click', (e) => {
+            const num = e.currentTarget.id;
+            myLibrary.splice(num, 1);
+            render();
+        })
+        tableRow.append(tableData);
+        tableData.append(removeBtn);
 
-        const buttonRead = document.createElement('button');
-        buttonRead.textContent = 'Read';
-        buttonRead.className = 'read-button';
-        buttonRead.addEventListener('click', handleButtonClickRead);
-
-        const buttonRemove = document.createElement('button');
-        buttonRemove.textContent = 'Remove';
-        buttonRemove.className = 'remove-button';
-        buttonRemove.id = numIndex;
-        numIndex += 1; 
-        buttonRemove.addEventListener('click', handleButtonClickRemove);
-
-        tableRow.append(colOneData);
-        colOneData.append(buttonRead, buttonRemove);
-
-        const value = Object.values(myLibrary[j]);
-        for(let i = 0; i < 4; i++){
-            const tabledata1 = document.createElement('td');
-            tabledata1.textContent = value[i];
-            tableRow.append(tabledata1);
+        //Book data add to row
+        const values = Object.values(myLibrary[row]);
+        for(let col = 0; col < 5; col++) {
+            const tableDataBook = document.createElement('td');
+            tableDataBook.textContent = values[col];
+            tableRow.append(tableDataBook);
         }
     }
 }
 
-function handleButtonClickRead(event){
-    const btn = event.currentTarget
-    if(isClicked === false){
-        btn.style.backgroundColor = 'green';
-        isClicked = true;
-    }
-    else{
-        btn.style.backgroundColor = 'white';
-        isClicked = false;
-    }
-}
-
-function handleButtonClickRemove(event){
-    const item = event.currentTarget.id;
-    myLibrary.splice(item, 1);
-    makeTable();
-}
-
-const openBtn = document.querySelector('#new-book');
-const dialog = document.querySelector('#dialog-book');
-
-openBtn.addEventListener('click', () => {
-    dialog.showModal();
-})
-
-const submitBtn = document.querySelector('#submitBtn');
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const year = document.querySelector('#year');
-
-submitBtn.addEventListener('click', ()=>{
-    addBookToLibrary(title.value, author.value, year.value);
-})
-
-
-
-addBookToLibrary("Harry Potter and the Half-Blood Prince", "J.K. Rowling", 2005);
-addBookToLibrary("Maze Runner", "James Dashner", 2009);
+//Default book parameters and render them initailly 
+const book1 = new Book('d', 'dd', 2005, 'yes');
+const book2 = new Book('e', 'ee', 2005, 'no');
+const book3 = new Book('f', 'ff', 2005, 'yes');
+myLibrary.push(book1, book2, book3);
+render();
 
